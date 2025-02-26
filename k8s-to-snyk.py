4986d8d544e3c94be4c896101527817948e583a6
Torsten Cannell <38391283+Torsten1014@users.noreply.github.com>
2025-02-26 15:24:25 -0800

    feat: enable default org to be used in cases where mapping doesnt exist

--- k8s-to-snyk.py
+++ k8s-to-snyk.py
@@ -3,13 +3,12 @@ import json
 import re
 from kubernetes import client, config
 import yaml
-import os
 import argparse
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
-logger = logging.getLogger("kubectl-images")
+logger = logging.getLogger("k8s-to-snyk")
 
 
 class Client:
@@ -21,12 +20,6 @@ class Client:
     def _load_config_file(self, config_file):
         """Loads configuration data from a JSON or YAML file."""
         try:
-            if not os.path.exists(config_file):
-                logger.warning(
-                    f"Config file {config_file} not found. Using default empty config."
-                )
-                return {}
-
             with open(config_file, "r") as f:
                 if config_file.endswith(".json"):
                     return json.load(f)
@@ -62,37 +55,58 @@ class Client:
             logger.error(f"Error loading Kubernetes configuration: {e}")
             raise
 
-    def get_all_images(self, namespaces=None):
-        """Fetches all image names across specified namespaces or all if none are provided."""
+    def _get_image_metadata_from_pods(self, pods):
+        """Fetches image metadata (image name and mapping label value) for a pod"""
         try:
-            all_images = set()
-            if namespaces:
-                for namespace in namespaces:
-                    pods = self.api.list_namespaced_pod(
-                        namespace=namespace, watch=False
-                    )
-                    for pod in pods.items:
-                        for container in pod.spec.containers:
-                            all_images.add(container.image)
-                        if pod.spec.init_containers:
-                            for init_container in pod.spec.init_containers:
-                                all_images.add(init_container.image)
-            else:
-                pods = self.api.list_pod_for_all_namespaces(watch=False)
-                for pod in pods.items:
-                    for container in pod.spec.containers:
-                        all_images.add(container.image)
-                    if pod.spec.init_containers:
-                        for init_container in pod.spec.init_containers:
-                            all_images.add(init_container.image)
+            images = {}
+            mapping = self.config_data.get("snyk_org_mapping")
+            mapping_label = ""
+            if mapping.get("map_on") == "label":
+                mapping_label = mapping.get("label")
 
-            return sorted(list(all_images))
+            for pod in pods.items:
+                label_value = ""
+                if mapping_label:
+                    labels = pod.metadata.labels
+                    label_value = labels.get(mapping_label)
+
+                for container in pod.spec.containers:
+                    image_name = container.image
+                    images[image_name] = {"namespace": pod.metadata.namespace}
+                    images[image_name].update({"label": label_value})
+
+                if pod.spec.init_containers:
+                    for init_container in pod.spec.init_containers:
+                        image_name = init_container.image
+                        images[image_name] = {"namespace": pod.metadata.namespace}
+                        images[image_name].update({"label": label_value})
+            return images
 
         except client.exceptions.ApiException as e:
             logger.error(f"Error fetching pod information: {e}")
             return None
         except Exception as e:
             logger.exception(f"An unexpected error occurred: {e}")
+            return None
+
+    def get_all_image_metadata(self, namespaces=None):
+        """Fetches all image names and labels across specified namespaces or all if none are provided."""
+        try:
+            images = {}
+            if namespaces:
+                for namespace in namespaces:
+                    pods = self.api.list_namespaced_pod(
+                        namespace=namespace, watch=False
+                    )
+                    pod_images = self._get_image_metadata_from_pods(pods)
+                    images.update(pod_images)
+            else:
+                pods = self.api.list_pod_for_all_namespaces(watch=False)
+                pod_images = self._get_image_metadata_from_pods(pods)
+                images.update(pod_images)
+            return images
+        except Exception as e:
+            logger.exception(f"Error getting image metadata: {e}")
             return None
 
     def filter_images(self, images, exclude_regex):
@@ -108,67 +122,44 @@ class Client:
             logger.error(f"Invalid regex pattern: {e}")
             return images
 
-    def map_image_to_snyk(self, namespace, image_name):
+    def map_image_to_snyk(self, image):
         """Maps an image to Snyk org and integration ID based on config."""
+        image_name = image[0]
         snyk_mapping = self.config_data.get("snyk_org_mapping", {})
         if not snyk_mapping:
             return None
 
+        snyk_data = None
         map_on = snyk_mapping.get("map_on")
         if map_on == "label":
-            label_name = snyk_mapping.get("label")
-            pod_labels = self.get_image_labels(namespace, image_name)
-            if not pod_labels:
+            label_value = image[1].get("label")
+            snyk_data = snyk_mapping.get("values").get(label_value)
+
+        elif map_on == "namespace":
+            namespace_value = image[1].get("namespace")
+            snyk_data = snyk_mapping.get("values").get(namespace_value)
+
+        if snyk_data:
+            return {
+                "image": image_name,
+                "orgId": snyk_data.get("snyk_org_id"),
+                "integrationId": snyk_data.get("snyk_integration_id"),
+            }
+
+        else:
+            default = snyk_mapping.get("default")
+            if default:
+                default_mapping = {
+                    "image": image_name,
+                    "orgId": default.get("snyk_org_id"),
+                    "integrationId": default.get("snyk_integration_id"),
+                }
+                return default_mapping
+            else:
                 return None
 
-            for pod_name, labels in pod_labels.items():
-                label_value = labels.get(label_name)
-                if label_value and snyk_mapping.get("values"):
-                    for map_value, snyk_data in snyk_mapping["values"].items():
-                        if label_value == map_value:
-                            return {
-                                "orgId": snyk_data.get("snyk_org_id"),
-                                "integrationId": snyk_data.get("snyk_integration_id"),
-                            }
-        elif map_on == "namespace":
-            if snyk_mapping.get("values"):
-                for map_value, snyk_data in snyk_mapping["values"].items():
-                    if namespace == map_value:
-                        return {
-                            "orgId": snyk_data.get("snyk_org_id"),
-                            "integrationId": snyk_data.get("snyk_integration_id"),
-                        }
-
-        return None
-
-    def get_image_labels(self, namespace, image_name):
-        """Fetches labels associated with pods using a specific image."""
-        try:
-            pod_labels = {}
-            pods = self.api.list_namespaced_pod(namespace=namespace, watch=False)
-
-            for pod in pods.items:
-                for container in pod.spec.containers:
-                    if container.image == image_name:
-                        pod_labels[pod.metadata.name] = pod.metadata.labels or {}
-                if pod.spec.init_containers:
-                    for init_container in pod.spec.init_containers:
-                        if init_container.image == image_name:
-                            pod_labels[pod.metadata.name] = pod.metadata.labels or {}
-
-            return pod_labels
-
-        except client.exceptions.ApiException as e:
-            logger.error(
-                f"Error fetching pod labels for image {image_name} in namespace {namespace}: {e}"
-            )
-            return None
-        except Exception as e:
-            logger.exception(f"An unexpected error occurred: {e}")
-            return None
-
     def create_targets_file(self, images_data, output_file="imported-targets.json"):
-        """Generates targets file"""
+        """Generates targets file for use by API Import Tool"""
         targets = []
         for image_data in images_data:
             targets.append(
@@ -203,26 +194,18 @@ if __name__ == "__main__":
         client = Client(kube_config=args.kube_config, config_file=args.config)
 
         namespaces_to_scan = client.config_data.get("namespaces")
-        all_images = client.get_all_images(namespaces=namespaces_to_scan)
+        all_images = client.get_all_image_metadata(namespaces=namespaces_to_scan)
         exclude_regex = client.config_data.get("image_filter_regex_exclude")
         filtered_images = client.filter_images(all_images, exclude_regex)
 
-        images_data = []
-        for image in filtered_images:
-            for namespace in namespaces_to_scan:
-                snyk_mapping = client.map_image_to_snyk(namespace, image)
-                if snyk_mapping:
-                    images_data.append(
-                        {
-                            "image": image,
-                            "orgId": snyk_mapping["orgId"],
-                            "integrationId": snyk_mapping["integrationId"],
-                        }
-                    )
-                    break
+        targets = []
+        for image in filtered_images.items():
+            target = client.map_image_to_snyk(image)
+            if target:
+                targets.append(target)
 
         client.create_targets_file(
-            images_data, client.config_data.get("targets_file_output_path")
+            targets, client.config_data.get("targets_file_output_path")
         )
 
     except Exception as e:
